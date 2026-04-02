9b511db2a1d49552ab7a6281b396a6623605cc41
idokoken <idokokennethnduka@gmail.com>
2026-04-02 18:03:23 +0100

    updated

---
+++ bways-frontend/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+// import heroImg from './assets/hero.png'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+// import Cart from "./pages/Cart";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import ErrorPage from "./pages/ErrorPage";
+// import Product from "./components/Product";
+// import Products from "./components/Products";
+// import PrivacyPolicy from "./pages/PrivacyPolicy"
+// import TermsAndConditions from "./pages/TermsAndConditions"
+// import ScrollToTop from './ScrollToTop';
+// import ProductsPage from "./pages/ProductsPage";
+// import SingleProduct from "./pages/SingleProduct";
+// import Orders from "./pages/Order"
+// import Collection from "./components/Collection";
+// import PlaceOrder from "./components/PlaceOrder"
+
+
+function App() {
+
+
+  return (
+    <>
+      {/* <div>
+
+        <h2>Documentation</h2>
+        <p>Your questions, answered</p>
+
+      </div> */}
+
+      <Router>
+        {/* <ScrollToTop /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/about-us" element={<About />} />
+          {/* <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+        
+        
+        <Route path="/collection" exact element={<Collection />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+
+        <Route path="/signin" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="*" element={<ErrorPage />} /> */}
+        </Routes>
+      </Router>
+
+    </>
+  )
+}
+
+export default App
